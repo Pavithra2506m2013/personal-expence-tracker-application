000b057295c50424833483fe3efb466715942255
Pavithra2506m2013 <118714929+Pavithra2506m2013@users.noreply.github.com>
2022-11-23 09:27:16 -0800

    Add files via upload

---
+++ Team leader PAVITHRA M/Assignment_2/login.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS `login` DEFAULT CHARACTER SET utf8mb3;
+USE `login`;
+
+CREATE TABLE IF NOT EXISTS `accounts`(`id` int(11) NOT NULL AUTO_INCREMENT, 
+`username` varchar(50) NOT NULL, 
+`password` varchar(255) NOT NULL,
+`email` varchar(100) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB auto_increment=2 DEFAULT CHARSET=utf8;
